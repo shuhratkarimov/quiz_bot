3d08b9b234cf89829a31610707ef34b36dfed4d3
shuhratkarimov <shuhratkarimov.dev@gmail.com>
2025-03-18 21:12:19 +0500

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { BotModule } from './bot/bot.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { BotModule } from "./bot/bot.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({envFilePath: ".env", isGlobal: true}),
-    MongooseModule.forRoot(process.env.MONGO_URI as string),
-    BotModule
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    BotModule,
   ],
 })
 export class AppModule {}
